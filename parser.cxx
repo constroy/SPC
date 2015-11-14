01b14e99b5e2412a549844dbcadeb14f61432590
constroy <constroy.li@gmail.com>
2015-11-15 00:03:15 +0800

    add class parser

---
+++ parser.cxx
@@ -0,0 +1,18 @@
+/*
+ * parser.cxx
+ *
+ * Copyright 2015 constroy <constroy.li@gmail.com>
+ *
+ *
+ */
+
+
+#include "parser.hxx"
+
+
+Parser::Parser()
+{
+
+}
+
+
---
+++ parser.hxx
@@ -0,0 +1,22 @@
+/*
+ * parser.hxx
+ *
+ * Copyright 2015 constroy <constroy.li@gmail.com>
+ *
+ *
+ */
+
+
+#ifndef PARSER_HXX
+#define PARSER_HXX
+
+class Parser
+{
+	public:
+		Parser();
+
+	private:
+		/* add your private declarations */
+};
+
+#endif /* PARSER_HXX */
