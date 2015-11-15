b04384745b5617bed36aaeafc6b349a5aad8e472
constroy <constroy.li@gmail.com>
2015-11-15 22:23:03 +0800

    complete unit test for Lexer

--- spc.cxx
+++ spc.cxx
@@ -8,6 +8,7 @@
 
 #include <cstdio>
 #include "lexer.hxx"
+#include <set>
 
 int main(int argc,char *argv[])
 {
@@ -17,37 +18,29 @@ int main(int argc,char *argv[])
 		exit(-1);
 	}
 	Token token;
-	Lexer lexer(fopen(argv[1],"r"));
-	//code for test-----------------------------------------
+	Lexer lexer(argv[1]);
+	//unit test [Lexer]------------------------------------------------
+	const std::string type[]=
+	{
+		"ident",
+		"number","character","string",
+		"readsym","writesym",
+		"intsym","charsym","arraysym",
+		"constsym","varsym","funcsym","procsym",
+		"become","plus","minus","times","slash","eql","neq","lss","leq","gtr","geq",
+		"lbracket","rbracket","lparen","rparen","colon","comma","semicolon","period",
+		"beginsym","endsym","dosym","whilesym","forsym","tosym","downtosym","ifsym","thensym","elsesym"
+	};
 	freopen(argv[2],"w",stdout);
 	do
 	{
 		token=lexer.NextToken();
-		if (token.type==ident)
-		{
-			printf("ident\t%s\n",token.s.c_str());
-		}
-		else if (token.type==number)
-		{
-			printf("number\t%d\n",token.v);
-		}
-		else if (token.type==character)
-		{
-			printf("character\t%c\n",token.v);
-		}
-		else if (token.type==string)
-		{
-			printf("string\t%s\n",token.s.c_str());
-		}
-		else
-		{
-			for (auto i:word) if (token.type==i.second)
-				printf("%s\t%s\n",i.first.c_str(),token.s.c_str());
-			for (auto i:symb) if (token.type==i.second)
-				printf("%s\t%s\n",i.first.c_str(),token.s.c_str());
-		}
+		printf("%-12s",type[token.type].c_str());
+		if (token.type==number) printf("%d\n",token.v);
+		else if (token.type==character)	printf("%c\n",token.v);
+		else printf("%s\n",token.s.c_str());
 	}
 	while (token.type!=period);
-	//------------------------------------------------------
+	//-----------------------------------------------------------------
 	return 0;
 }
