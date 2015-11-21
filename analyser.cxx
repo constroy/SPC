960c44a64c50602876af1c729af598d4fa6cef2f
constroy <constroy.li@gmail.com>
2015-11-21 23:03:52 +0800

    create Analyser

---
+++ analyser.cxx
@@ -0,0 +1,17 @@
+/*
+ * analyser.cxx
+ *
+ * Copyright 2015 constroy <constroy.li@gmail.com>
+ *
+ *
+ */
+
+
+#include "analyser.hxx"
+
+Analyser::Analyser()
+{
+
+}
+
+
---
+++ analyser.hxx
@@ -0,0 +1,22 @@
+/*
+ * analyser.hxx
+ *
+ * Copyright 2015 constroy <constroy.li@gmail.com>
+ *
+ *
+ */
+
+
+#ifndef ANALYSER_HXX
+#define ANALYSER_HXX
+
+class Analyser
+{
+	public:
+		Analyser();
+
+	private:
+		/* add your private declarations */
+};
+
+#endif /* ANALYSER_HXX */
