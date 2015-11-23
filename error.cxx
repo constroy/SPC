47fed75af64a490d82cdf09347cd81b68abbc199
constroy <constroy.li@gmail.com>
2015-11-24 00:03:45 +0800

    merge error_msg and warning_msg, add new messages

--- error.cxx
+++ error.cxx
@@ -7,24 +7,44 @@
 
 #include "error.hxx"
 
-const char error_msg[][64]=
+const char msg[][64]=
 {
+	//lexer
 	"cannot open the source file",
 	"unexpected end of file",
-	"unknown charactor or symbol"
-};
-
-const char warning_msg[][64]=
-{
+	"unknown charactor or symbol",
 	"constant integer may overflow",
+	//parser
+	"identifier expected",
+	"number expected",
+	"number or character expected",
+	"'of' expected",
+	"type expected",
+	"'begin' expected",
+	"'while' expected",
+	"':=' expected",
+	"'to' or 'downto' expected",
+	"'do' expected",
+	"'then' expected",
+	"'end' expected",
+
+	"'.' expected",
+	"':' expected",
+	"';' expected",
+	"'(' expected",
+	"')' expected",
+	"'[' expected",
+	"']' expected",
+	"'=' expected",
+	"relational operator expected",
+
 };
 
-void error(int r,int c,Error e)
+void error(Pos pos,Error e)
 {
-	fprintf(stderr,"r%dc%d: error: %s",r,c,error_msg[e]);
+	fprintf(stderr,"r%dc%d: error: %s",pos.first,pos.second,msg[e]);
 }
-
-void warning(int r,int c,Warning w)
+void warning(Pos pos,Error w)
 {
-	fprintf(stderr,"r%dc%d: warning: %s",r,c,warning_msg[w]);
+	fprintf(stderr,"r%dc%d: warning: %s",pos.first,pos.second,msg[w]);
 }
--- error.hxx
+++ error.hxx
@@ -11,22 +11,46 @@
 #define ERROR_HXX
 
 #include <cstdio>
+#include <utility>
+
+typedef std::pair<int,int> Pos;
 
 enum Error
 {
+	//lexer
 	open_failed,
 	unexpected_EOF,
-	unknown_character
-};
-enum Warning
-{
-	integer_overflow
+	unknown_character,
+	integer_overflow,
+	//parser
+	lost_ident,
+	lost_number,
+	lost_num_or_char,
+	lost_of,
+	lost_type,
+	lost_begin,
+	lost_while,
+	lost_assign,
+	lost_to,
+	lost_do,
+	lost_then,
+	lost_end,
+
+	lost_period,
+	lost_colon,
+	lost_semicolon,
+	lost_lbracket,
+	lost_rbracket,
+	lost_lparen,
+	lost_rparen,
+	lost_eql,
+	lost_relational,
+
 };
 
-extern const char error_msg[][64];
-extern const char warning_msg[][64];
+extern const char msg[][64];
 
-void error(int r,int c,Error e);
-void warning(int r,int c,Warning w);
+void error(Pos pos,Error e);
+void warning(Pos pos,Error w);
 
 #endif /* ERROR_HXX */
